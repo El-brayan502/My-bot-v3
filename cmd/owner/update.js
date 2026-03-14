3ba2935323dbfa8b4880d25c4be0065042aa10d1
Brayan Uchiha <brayanphonkponk@gmail.com>
2026-03-13 23:19:02 -0600

    Actualizar update.js

--- cmd/owner/update.js
+++ cmd/owner/update.js
@@ -8,11 +8,9 @@ let handler = async (m, { conn, text }) => {
     const cmd = 'git pull' + (m.fromMe && text ? ' ' + text : '')
     const out = execSync(cmd, { encoding: 'utf-8' })
 
-    await conn.reply(
-      m.chat,
-      `🛠️ *ACTUALIZACIÓN DEL BOT*\n\n${out.trim() || '✅ Actualizado correctamente'}`,
-      m
-    )
+    await conn.sendMessage(m.chat, {
+      text: `🛠️ *ACTUALIZACIÓN DEL BOT*\n\n${out.trim() || '✅ Actualizado correctamente'}`
+    }, { quoted: m })
 
     await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
 
@@ -20,11 +18,10 @@ let handler = async (m, { conn, text }) => {
 
     await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } })
 
-    await conn.reply(
-      m.chat,
-      `⚠️ *Error al actualizar*\n\n${e.message}`,
-      m
-    )
+    await conn.sendMessage(m.chat, {
+      text: `⚠️ *Error al actualizar*\n\n${e.message}`
+    }, { quoted: m })
+
   }
 }
 
